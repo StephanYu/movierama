69344704d2193ec5c84f1af5aaf0801d9869e68d
Stephan Yue <syue@Stephans-Mac-mini.local>
2016-04-14 16:51:17 +0100

    add skeleton spec files for jobs and mailers

---
+++ spec/jobs/send_email_job_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+Rspec.describe 'job to send email with delay', type: :feature do
+
+end
---
+++ spec/mailers/user_mailer_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+Rspec.describe 'send email to user', type: :feature do
+
+end
